a0a5f3d3bc7b749b7db7b3c6375b8d8916957eb4
Ritik Raj <75975086+iamtheritikraj@users.noreply.github.com>
2021-08-05 09:50:22 +0530

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -4,15 +4,11 @@ var height=document.getElementById("height-input").value;
 var weight=document.getElementById("weight-input").value;
 var heightunit=document.getElementById("unit-height").value;
 var weightunit=document.getElementById("unit-weight").value;
-
-alert("Enter Required Value");
 if(heightunit=="in-inch")
 height=height*0.0254;
 if(weightunit=="in-pound")
 weight=weight*0.453592;
 var bmi=weight/(height*height);
-if(bmi==NaN)
-alert("Enter Value");
 console.log(height);
 console.log(weight);
 console.log(bmi);
